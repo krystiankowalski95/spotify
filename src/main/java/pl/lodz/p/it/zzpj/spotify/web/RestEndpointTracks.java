23159a198298d52fdd2f9145ff3fbae6206c604c
216808 Krystian Kowalski <216808@edu.p.lodz.pl>
2020-06-13 08:31:36 +0200

    Refactor, próba stworzenia strony z listą utworów

--- src/main/java/pl/lodz/p/it/zzpj/spotify/web/RestEndpointTracks.java
+++ src/main/java/pl/lodz/p/it/zzpj/spotify/web/RestEndpointTracks.java
@@ -1,5 +1,7 @@
 package pl.lodz.p.it.zzpj.spotify.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -12,13 +14,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import pl.lodz.p.it.zzpj.spotify.model.Item;
 import pl.lodz.p.it.zzpj.spotify.model.Tracks;
+import pl.lodz.p.it.zzpj.spotify.proxy.PlaylistsProxy;
+import pl.lodz.p.it.zzpj.spotify.proxy.TracksProxy;
+import pl.lodz.p.it.zzpj.spotify.services.PlaylistService;
+import pl.lodz.p.it.zzpj.spotify.services.TrackService;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @RestController
 public class RestEndpointTracks {
 
+    @Autowired
+    TrackService trackService;
+    @Autowired
+    TracksProxy tracksProxy;
+
+
+
     @GetMapping("/track/")
-    public ModelAndView getTracks(OAuth2Authentication details, @RequestParam("searchPhrase")String phrase) {
+    public Object getTracks(OAuth2Authentication details, @RequestParam("name")String phrase) {
         String jwt = ((OAuth2AuthenticationDetails)details.getDetails()).getTokenValue();
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders httpHeaders = new HttpHeaders();
@@ -26,10 +44,26 @@ public class RestEndpointTracks {
         HttpEntity httpEntity = new HttpEntity(httpHeaders);
 
 
-        ResponseEntity<Object> response = restTemplate.exchange("https://api.spotify.com/v1/search?q="+phrase+"&limit=50&type=track",
+        LinkedHashMap response = ((LinkedHashMap)restTemplate.exchange("https://api.spotify.com/v1/search?q="+phrase+"&limit=3&type=track",
                 HttpMethod.GET,
-                httpEntity,Object.class);
-        return new ModelAndView("tracksView", "tracks", Tracks.makeTracksFromResponseEntity(response));
-//        return response;
+                httpEntity,Object.class).getBody());
+
+
+        LinkedHashMap items = (LinkedHashMap) response.get("tracks");
+        LinkedHashMap item = (LinkedHashMap) items.get("items");
+        System.out.println(item);
+        return response.get("tracks");
+
+//        List<Integer> possitions = TempUtils.getRandomFromRangeUnreapeated(playlistTrackBodyItems.size(),5);
+//        List<String> baseTrackNames = new ArrayList<>();
+//
+//        for (Integer possition: possitions) {
+//            baseTrackNames.add((String) ((LinkedHashMap) ((LinkedHashMap) playlistTrackBodyItems.get(possition))
+//                    .get("track")).get("id"));
+//        }
+
+//        return Item.makeItemsFromResponseEntity(response);
     }
+
+
 }
