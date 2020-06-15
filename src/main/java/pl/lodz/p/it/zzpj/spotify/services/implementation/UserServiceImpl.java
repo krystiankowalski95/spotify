f6937b253730e7b9c38eb01f0cbce8f063a6f248
Szymon Gruda <216773@edu.p.lodz.pl>
2020-06-15 19:25:39 +0200

    Dodanie brakujących zmian.

--- src/main/java/pl/lodz/p/it/zzpj/spotify/services/implementation/UserServiceImpl.java
+++ src/main/java/pl/lodz/p/it/zzpj/spotify/services/implementation/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package pl.lodz.p.it.zzpj.spotify.services.implementation;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 import pl.lodz.p.it.zzpj.spotify.HttpConfiguration;
+import pl.lodz.p.it.zzpj.spotify.model.User;
 import pl.lodz.p.it.zzpj.spotify.services.interfaces.UserService;
 
 import java.util.LinkedHashMap;
@@ -12,6 +14,18 @@ import java.util.LinkedHashMap;
 public class UserServiceImpl implements UserService {
 
     private HttpConfiguration httpConfiguration;
+
+    @Override
+    public User getCurrentUser(OAuth2Authentication details) {
+        this.httpConfiguration = new HttpConfiguration(details);
+        ResponseEntity responseEntity = httpConfiguration.getRestTemplate().exchange(
+                "https://api.spotify.com/v1/me",
+                HttpMethod.GET,
+                httpConfiguration.getHttpEntity(),
+                User.class
+        );
+        return (User) responseEntity.getBody();
+    }
 
     @Override
     public String getCurrentUserID(OAuth2Authentication details) {
--- src/main/java/pl/lodz/p/it/zzpj/spotify/services/interfaces/TrackService.java
+++ src/main/java/pl/lodz/p/it/zzpj/spotify/services/interfaces/TrackService.java
@@ -1,10 +1,7 @@
 package pl.lodz.p.it.zzpj.spotify.services.interfaces;
 
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import pl.lodz.p.it.zzpj.spotify.model.Item;
 import pl.lodz.p.it.zzpj.spotify.model.Tracks;
-
-import java.util.List;
 
 public interface TrackService {
     Tracks getTracks(OAuth2Authentication details, String phrase);
